ac2b4a31b94f71e01fa84014c0c90802fecf5615
awfuldynne <miller.sean.a@gmail.com>
2015-06-17 20:15:53 -0700

    Update ReadMe and run_analysis.R script

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
 # Getting and Cleaning Data - Course Project
 #### Sean Miller
 
-### Required Packages
+#### Required Packages
 * reshape2
 * dplyr
 
-### Script Preparation
-* run_analysis.R assumes that the contents of the zipfile located at https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip has been unpacked in your current working directory.
-* If either reshape2 or dplyr packages are not installed, run_analysis.R will attempt to install them.
+#### Script Preparation
+* run_analysis.R assumes that the contents of the zipfile located at https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip have been unpacked in your current working directory.
+* If either the reshape2 or dplyr package isn't installed, run_analysis.R will attempt to install them.
 
-### Running the Script
-1. 
+#### What the Script Does
+1. Each set of data (test, training) is made up of three parts
+	* X: The values of each measurement
+	* subject: Which subject performed the measurement
+	* activity/y: The activity during which the measurement was taken
+2. The variable names are taken from the file features.txt and mapped to the corresponding column of X (e.g. tBodyAcc-mean()-X)
+3. Subject, activity/y and X are then column bound together to build the respective data set.
+	* During this step, the individual data set is joined to the list of activities from the file activity_labels.txt.
+4. After both the test and training sets of data have been assembled, the script finds all column names with either the string "std()" or "mean()" in them.
+5. The test and training data sets are then row bound together, while reducing the columns to the list of columns created in step 4.
+6. The data set is then pivoted with the melt function into the form (subject, activity, variable, value), where variable maps to one of the variable names of either a mean or std deviation of a measurement and value is the value of that measurement.
+7. The data is grouped by (subject, activity, variable) and summarized such that the value is the mean of that variable for that subject and activity.
+8. The output of the data set is written to "./tidy_data_output.txt" and loaded via View in R.
 
-### Reading the Data
+#### Reading the Data
 Data can be read in R with the following command:
-* read.table("./getdata-015-project/tidy_data_output.txt", header = TRUE)
+* read.table("./tidy_data_output.txt", header = TRUE)
--- run_analysis.R
+++ run_analysis.R
@@ -6,67 +6,67 @@ library(reshape2)
 library(dplyr)
 
 # Read in feature list
-feature_file        = "./getdata-015-project/UCI HAR Dataset/features.txt"
+feature_file        = "./UCI HAR Dataset/features.txt"
 feature     = read.table(feature_file)
 col_names   = feature[,2]
 
 # Read in activity labels
-activity_labels_file = "./getdata-015-project/UCI HAR Dataset/activity_labels.txt"
+activity_labels_file = "./UCI HAR Dataset/activity_labels.txt"
 activity_labels = read.table(activity_labels_file)
 
 ## TEST DATA ##
 # Read in and combine the test data set
-testdata_X          = "./getdata-015-project/UCI HAR Dataset/test/X_test.txt"
-testdata_subject    = "./getdata-015-project/UCI HAR Dataset/test/subject_test.txt"
-testdata_y          = "./getdata-015-project/UCI HAR Dataset/test/y_test.txt"
+testdata_X          = "./UCI HAR Dataset/test/X_test.txt"
+testdata_subject    = "./UCI HAR Dataset/test/subject_test.txt"
+testdata_y          = "./UCI HAR Dataset/test/y_test.txt"
 
 td_x        = read.table(testdata_X)
 td_subject  = read.table(testdata_subject)
 td_y        = read.table(testdata_y)
 
-##   Use only descriptive activity names to name the activities
-td_y_merged     = merge(td_y, activity_labels, by.x="V1", by.y="V1")
-td_activity     = td_y_merged[2]
-
 ##   Appropriately label the data set with descriptive variable names
 # Name test data columns
 names(td_x) = col_names
 names(td_subject) = "subject" 
-names(td_activity) = "activity"
+names(td_y) = "activity"
 
 # Bind subject and activity columns to data
-td = cbind(td_subject, td_activity, td_x)
+td_raw = cbind(td_subject, td_y, td_x)
+
+##   Use only descriptive activity names to name the activities
+td_merged       = merge(td_raw, activity_labels, by.x="activity", by.y="V1")
+td_merged$activity = td_merged$V2
 
 ## TRAINING DATA ##
 # Read in and combine the training data set
-trdata_X          = "./getdata-015-project/UCI HAR Dataset/train/X_train.txt"
-trdata_subject    = "./getdata-015-project/UCI HAR Dataset/train/subject_train.txt"
-trdata_y          = "./getdata-015-project/UCI HAR Dataset/train/y_train.txt"
+trdata_X          = "./UCI HAR Dataset/train/X_train.txt"
+trdata_subject    = "./UCI HAR Dataset/train/subject_train.txt"
+trdata_y          = "./UCI HAR Dataset/train/y_train.txt"
 
 tr_x        = read.table(trdata_X)
 tr_subject  = read.table(trdata_subject)
 tr_y        = read.table(trdata_y)
 
-##   Use only descriptive activity names to name the activities
-tr_y_merged     = merge(tr_y, activity_labels, by.x="V1", by.y="V1")
-tr_activity     = tr_y_merged[2]
-
 ##   Appropriately label the data set with descriptive variable names
 # Name training data columns
 names(tr_x) = col_names
 names(tr_subject) = "subject" 
-names(tr_activity) = "activity"
+names(tr_y) = "activity"
 
 # Bind subject and activity columns to data
-tr = cbind(tr_subject, tr_activity, tr_x)
+tr_raw = cbind(tr_subject, tr_y, tr_x)
+
+##   Use only descriptive activity names to name the activities
+tr_merged       = merge(tr_raw, activity_labels, by.x="activity", by.y="V1")
+tr_merged$activity = tr_merged$V2
 
 ## Extract only the mean and std dev for each measurement
 # Find all columns that contain mean() or std()
-columns_to_keep = sort(c(1, 2, grep("std()", names(td)), grep("mean()", names(td))))
-colnames_to_keep = names(td)[sort(c(grep("std()", names(td)), grep("mean()", names(td))))]
+columns_to_keep = sort(c(1, 2, grep("std()", names(td_merged)), grep("mean()", names(td_merged))))
+colnames_to_keep = names(td_merged)[sort(c(grep("std()", names(td_merged)), grep("mean()", names(td_merged))))]
 
 # Merge both data sets together
-data = rbind(td[, columns_to_keep], tr[, columns_to_keep])
+data = rbind(td_merged[, columns_to_keep], tr_merged[, columns_to_keep])
 
 ##    Create a new, independent and tidy data set with the mean of each variable
 ##    for each activity and each subject
@@ -75,4 +75,5 @@ melted_data = melt(data, id=c("subject", "activity"), measure.vars=colnames_to_k
 grouped_data = group_by(melted_data, subject, activity, variable)
 summary = summarize(grouped_data, average = mean(value))
 
-write.table(summary, file = "./getdata-015-project/tidy_data_output.txt", row.names = FALSE)
+write.table(summary, file = "./tidy_data_output.txt", row.names = FALSE)
+View(summary)
